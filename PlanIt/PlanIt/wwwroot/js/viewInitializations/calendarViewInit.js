6d6e675029cebfd036bb7077b7e67ebf2d32127c
dziems <danielzms@gmail.com>
2020-11-24 14:18:24 -0800

    some formatting for daily view number line

--- PlanIt/PlanIt/wwwroot/js/viewInitializations/calendarViewInit.js
+++ PlanIt/PlanIt/wwwroot/js/viewInitializations/calendarViewInit.js
@@ -2,4 +2,15 @@
     document.getElementById('day-view').checked = true;
 }
 
+function createEvent(title, color, duration) {
+    event = '<td class="event">';
+    event += ;
+
+    event += '</td>'
+
+}
+function displayAllEvents() {
+
+}
+
 selectDailyView();
