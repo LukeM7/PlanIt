ac7eb34c79b4c8c1018365752db5442af1c755c0
dziems <danielzms@gmail.com>
2020-11-26 21:03:22 -0800

    categories table now reads from a json rather than a string of titles, added color functionality, a little more file organization

--- PlanIt/PlanIt/wwwroot/js/Layout/_layoutInit.js
+++ PlanIt/PlanIt/wwwroot/js/Layout/_layoutInit.js
@@ -2,9 +2,33 @@
     document.getElementById('npi-calendar-page-input').checked = true;
 }
 
+var categoriesJSON = {
+    "categories": [
+        { "title": "General", "color": "LightCoral" },
+        { "title": "School", "color": "LightSalmon" },
+        { "title": "Internship", "color": "Lavender" },
+        { "title": "Soccer", "color": "MediumPurple" },
+        { "title": "PT", "color": "LightGreen" },
+        { "title": "Doctor", "color": "Olive" },
+        { "title": "Chores", "color": "LightSeaGreen" },
+        { "title": "Dates<3", "color": "LightSkyBlue" },
+        { "title": "I dunno", "color": "Peru" }
+    ]
+};
 
-var categoryTitles = ["General", "School", "Soccer", "Personal", "A", "B", "C", " D", "E", "F", "G", "AA", "AB", "AC", "AD", "AE", "AF", "AG"];
-createCategoriesTable(categoryTitles);
+function initToggleAll(categoriesJSON) {
+    var toggleAll = document.getElementById('toggle-all-categories-input');
+    toggleAll.checked = true;
+    document.getElementById('toggle-all-label-text').innerHTML = 'Toggle All Off';
+    toggleAll.addEventListener('change', function () {
+        toggleAllCategories(this, categoriesJSON);
+    })
+    
+}
+
+
+buildCategoriesTable(categoriesJSON);
+initToggleAll(categoriesJSON);
 selectCalendarPage();
 
 
