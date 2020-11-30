216da4b25a02178024fd899a354f72b5eb5f1eb6
Luke Manzitto <lukemanzitto@gmail.com>
2020-11-30 01:25:22 -0800

    Updated Models to have correct forign key info and basic test constructors. Work will now be focused on implementing DB updates

--- PlanIt/PlanIt/Models/Category_Model.cs
+++ PlanIt/PlanIt/Models/Category_Model.cs
@@ -9,16 +9,22 @@ namespace PlanIt.Models
 {
     public class Category_Model
     {
+        public Category_Model()
+        {
+            this.Category_Id = "0";
+            this.Title = "0";
+            this.Events = new List<Event_Model>();
+            this.Color = "0";
+            this.Calendar_Id = "0";
+        }
+
         [Key]
         public string Category_Id { get; set; }
         public string Title { get; set; }
         public string Color { get; set; }
-        public List<Entry_Model> Entries { get; set; }
+        public List<Event_Model> Events { get; set; }
 
+        [ForeignKey("Calender_Model")]
         public string Calendar_Id { get; set; }
-
-        [ForeignKey("Calender_Id")]
-        public Calendar_Model Calendar { get; set; }
-
     }
 }
--- PlanIt/PlanIt/Models/User_Model.cs
+++ PlanIt/PlanIt/Models/User_Model.cs
@@ -8,6 +8,15 @@ namespace PlanIt.Models
 {
     public class User_Model
     {
+        public User_Model()
+        {
+            this.User_Id = "0";
+            this.Name = "Test";
+            this.Email = "0";
+            this.Phone_Number = "0";
+            this.Password = "0";
+        }
+
         [Key]
         public string User_Id { get; set; }
         public string Name { get; set; }
@@ -19,6 +28,5 @@ namespace PlanIt.Models
         public string Phone_Number { get; set; }
 
         public string Password { get; set; }
-        public Calendar_Model Calendar { get; set; }
     }
 }
