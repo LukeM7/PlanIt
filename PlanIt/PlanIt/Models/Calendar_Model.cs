89ae362dc81aaa1d0fa3f3a56d12b31cbf77626b
dziems <danielzms@gmail.com>
2020-11-30 02:09:45 -0800

    added some placeholder HttpPost actions for our controller that outline what will be encompassed in the actions. The next step is to figure out how to send values to these actions.

--- PlanIt/PlanIt/Models/Calendar_Model.cs
+++ PlanIt/PlanIt/Models/Calendar_Model.cs
@@ -9,21 +9,15 @@ namespace PlanIt.Models
 {
     public class Calendar_Model
     {
-
-        public Calendar_Model()
-        {
-            this.Calendar_Id = "0";
-            this.User_Id = "0";
-            this.Categories = new List<Category_Model>();
-        }
-
-
         [Key]
         public string Calendar_Id { get; set; }
 
-        [ForeignKey("User_Model")]
         public string User_Id { get; set; }
+
+        [ForeignKey("User_Id")]
+        public User_Model User { get; set; }
 
         public List<Category_Model> Categories { get; set; }
     }
+
 }
