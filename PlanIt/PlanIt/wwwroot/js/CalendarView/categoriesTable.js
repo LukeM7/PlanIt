0c606eaa588f351bea51ee32ade42552e4c67448
dziems <danielzms@gmail.com>
2020-12-03 17:37:20 -0800

    added event editing capability. Now updates model and re-displays events.

--- PlanIt/PlanIt/wwwroot/js/CalendarView/categoriesTable.js
+++ PlanIt/PlanIt/wwwroot/js/CalendarView/categoriesTable.js
@@ -8,6 +8,9 @@ function buildCategoriesTable(modelJSON) {
 
         buildCategoryRow(i, categoriesTable, category);
     }
+
+    //add another category
+
 }
 
 function buildCategoryRow(index, categoriesTable, category) {
@@ -61,7 +64,7 @@ function buildCategoryRow(index, categoriesTable, category) {
     editIcon.className = "far fa-edit";
 
     editBtn.addEventListener('click', function () {
-        showCategoryEditMenu(ctgUID, index, ctgTitle, ctgColor);
+        showCategoryCreatorMenu(index, category);
     });
 
     editBtn.appendChild(editIcon);
@@ -122,60 +125,61 @@ function toggleCategory(ctgUID, index, ctgCheckbox, ctgLabel, color) {
     });
 }
 
-function overwriteCategory(index, newTitle, newColor, ctgUID) {
+function overwriteCategory(index, category) {
     var categoriesTable = document.getElementById('categories-table');
+    alert(category.Color);
     categoriesTable.deleteRow(index);
-    buildCategoryRow(index, categoriesTable, newTitle, newColor, ctgUID);
+    buildCategoryRow(index, categoriesTable, category);
 }
 
 //pass in the index of the category from the json list of categories
 //pass in the label whose cosmetics will be edited   
-function showCategoryEditMenu(ctgUID, index, ctgTitle, ctgColor) {
-    var ctgRow = document.getElementById('category-table-row' + index.toString())
-    var menu = document.getElementById('edit-category-menu');
-    
+function showCategoryCreatorMenu(index, category) {
+    var menu = document.getElementById('category-creator-menu');
+    var ctgFirstRow = document.getElementById('category-table-row0');
     if (menu == null) {
-        menu = buildEditMenu(ctgUID, index, ctgTitle, ctgColor);
-        ctgRow.appendChild(menu);
+        menu = buildCreatorMenu(index, category);
+        ctgFirstRow.appendChild(menu);
     }
     else {
         //if menu's parent is not the same
-        if (menu.parentElement.id != ctgRow.id) {
-            menu.parentElement.ctgUID(menu);
-            menu = buildEditMenu(cgtUID, index, ctgTitle, ctgColor);
-            ctgRow.appendChild(menu);
+        if (menu.id != ('category-creator-menu' + index.toString()) ) {
+            menu.parentElement.removeChild(menu);
+            menu = buildCreatorMenu(index, category);
+            ctgFirstRow.appendChild(menu);
         }
         else {
             menu.parentElement.removeChild(menu);
         }
     }
 }
-function buildEditMenu(ctgUID, index, ctgTitle, ctgColor) {
+function buildCreatorMenu(index, category) {
     var menu = document.createElement('span');
-    menu.style.border = 'solid 2px ' + ctgColor;
-    menu.id = 'edit-category-menu';
+    menu.style.border = 'solid 2px ' + category.Color;
+    menu.className = 'category-creator-menu';
+    menu.id = 'category-creator-menu' + index.toString();
 
     
     //MENU TITLE AND EXIT BUTTON 
     var titleContainer = document.createElement('div');
-    titleContainer.id = 'edit-ctg-menu-title-container';
-    titleContainer.style.backgroundColor = ctgColor;
+    titleContainer.id = 'ctg-creatormenu-title-container';
+    titleContainer.style.backgroundColor = category.Color;
 
         var xButton = document.createElement('input');
-        xButton.id = 'edit-ctg-menu-exit-btn';
+        xButton.id = 'ctg-creatormenu-exit-btn';
         xButton.type = 'button';
         xButton.addEventListener('click', function () {
             menu.parentElement.removeChild(menu);
         });
 
         var xButtonLabel = document.createElement('label');
-        xButtonLabel.id = 'edit-ctg-menu-exit-label';
+        xButtonLabel.id = 'ctg-creatormenu-exit-label';
         xButtonLabel.innerHTML = '<span class="fas fa-times"></span>';
         xButtonLabel.htmlFor = xButton.id;
 
         var menuTitle = document.createElement('span');
-        menuTitle.id = 'edit-ctg-menu-title';
-        menuTitle.innerHTML = 'Edit ' + ctgTitle;
+        menuTitle.id = 'ctg-creatormenu-title';
+        menuTitle.innerHTML = 'Edit ' + category.Title;
 
 
     titleContainer.appendChild(xButton);
@@ -186,19 +190,19 @@ function buildEditMenu(ctgUID, index, ctgTitle, ctgColor) {
 
     //MENU EDITING OPTIONS 
     var editingClrContainer = document.createElement('div');
-    editingClrContainer.className = 'edit-ctg-menu-editing-container';
+    editingClrContainer.className = 'ctg-creatormenu-editing-container';
 
         var colorInput = document.createElement('input');
         colorInput.type = 'color';
-        colorInput.id = 'edit-ctg-menu-color-input';
-        colorInput.className = 'edit-ctg-menu-input';
-        colorInput.value = ctgColor;
+        colorInput.id = 'ctg-creatormenu-color-input';
+        colorInput.className = 'ctg-creatormenu-input';
+        colorInput.value = category.Color;
         colorInput.addEventListener('change', function () {
             const clr = this.value.toString();
-            var title = document.getElementById('edit-ctg-menu-title-container');
+            var title = document.getElementById('ctg-creatormenu-title-container');
             title.style.backgroundColor = clr;
-            var menu = document.getElementById('edit-category-menu');
-            menu.style.border = 'solid 2px ' + clr;
+            var ctgMenu = document.getElementById('category-creator-menu' + index.toString());
+            ctgMenu.style.border = 'solid 2px ' + clr;
         });
 
     editingClrContainer.appendChild(colorInput);
@@ -206,12 +210,12 @@ function buildEditMenu(ctgUID, index, ctgTitle, ctgColor) {
 
     if (index > 0) {
         var editingTitleContainer = document.createElement('div');
-        editingTitleContainer.className = 'edit-ctg-menu-editing-container';
+        editingTitleContainer.className = 'ctg-creatormenu-editing-container';
             var titleInput = document.createElement('input');
             titleInput.type = 'text';
-            titleInput.id = 'edit-ctg-menu-title-input';
-            titleInput.className = 'edit-ctg-menu-input';
-            titleInput.value = ctgTitle;
+            titleInput.id = 'ctg-creatormenu-title-input';
+            titleInput.className = 'ctg-creatormenu-input';
+            titleInput.value = category.Title;
 
         editingTitleContainer.appendChild(titleInput);
         menu.appendChild(editingTitleContainer);
@@ -219,24 +223,39 @@ function buildEditMenu(ctgUID, index, ctgTitle, ctgColor) {
 
     //MENU SAVE BUTTON 
     var saveContainer = document.createElement('div');
-    saveContainer.id = 'edit-ctg-menu-save-container';
+    saveContainer.id = 'ctg-creatormenu-save-container';
 
         var saveButton = document.createElement('input');
         saveButton.type = 'button';
-        saveButton.id = 'edit-ctg-menu-save-btn';
+        saveButton.id = 'ctg-creatormenu-save-btn';
         saveButton.value = 'Save Changes';
         saveButton.addEventListener('click', function () {
-            alert('write to model the new category values, also call an update to the events UI');
-
-            var newTitle;
-            if (index == 0) {
-                newTitle = 'General';
+            var ctgTitle;
+            if (index > 0) {
+                ctgTitle = document.getElementById('ctg-creatormenu-title-input').value;
             }
             else {
-                newTitle = document.getElementById('edit-ctg-menu-title-input').value;
+                ctgTitle = 'General';
             }
-            const newColor = document.getElementById('edit-ctg-menu-color-input').value;
-            overwriteCategory(index, newTitle, newColor, ctgUID);
+            const ind = index;
+            const ctgColor = document.getElementById('ctg-creatormenu-color-input').value;
+            $.ajax({
+                url: '/Calendar/EditCategory',
+                type: 'POST',
+                data: {
+                    index: ind,
+                    ctgTitle: ctgTitle,
+                    ctgColor: ctgColor,
+                },
+                success: function (result) {
+                    alert('success from editCategory!');
+                    var modelJSON = JSON.parse(result);
+                    alert(modelJSON.Categories[index].Color);
+                    overwriteCategory(index, modelJSON.Categories[index]);
+                    displayEvents(modelJSON, dateToString(currentDisplayedDate));
+                },
+            });
+            
         });
 
     saveContainer.appendChild(saveButton);
